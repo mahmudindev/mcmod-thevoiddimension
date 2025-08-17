a276bbfb25564d4b1a05996dbba138d47d439998
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-08-17 12:23:28 +0700

    Updates to 1.21

---
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/thevoiddimension/neoforge/TheVoidDimensionNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.mahmudindev.mcmod.thevoiddimension.neoforge;
+
+import com.github.mahmudindev.mcmod.thevoiddimension.TheVoidDimension;
+import net.neoforged.fml.common.Mod;
+
+@Mod(TheVoidDimension.MOD_ID)
+public final class TheVoidDimensionNeoForge {
+    public TheVoidDimensionNeoForge() {
+        // Run our common setup.
+        TheVoidDimension.init();
+    }
+}
